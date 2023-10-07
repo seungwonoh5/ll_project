a85b53a2e699a7d496522228f81bc6b2cd67e997
JOBIS7 <soh1@terpmail.umd.edu>
2023-10-07 11:07:15 -0400

    Created topic page that list all topics and a base template

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'learning_logs'
+app_name = 'learning_log'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+
 ]
--- learning_log/views.py
+++ learning_log/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Topic, Entry
 
 # Create your views here.
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_log/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    
+    return render(request, 'learning_log/topics.html', {'g_topics': topics})
